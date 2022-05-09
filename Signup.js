45f38a4530afe59632cb9bf03f9b0968e6ff78cf
hassan-prog <55570333+hassan-prog@users.noreply.github.com>
2022-05-09 07:51:01 +0200

    Add files via upload

---
+++ Signup.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { signup } from "../actions/auth";
+
+const Signup = ({ signup, isAuthenticated }) => {
+  const [accountCreated, setAccountCreated] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+    profile_type: "",
+  });
+
+  const { username, password, email, first_name, last_name, profile_type } =
+    formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    signup(username, password, email, first_name, last_name, profile_type);
+    setAccountCreated(true);
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />;
+  }
+  if (accountCreated) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1>Sign Up</h1>
+      <p>Create your Account</p>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Username*"
+            name="username"
+            value={username}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Password*"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength="6"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="email"
+            placeholder="Email*"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="First Name*"
+            name="first_name"
+            value={first_name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Last Name*"
+            name="last_name"
+            value={last_name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <select
+            className="form-control"
+            name="profile_type"
+            value="profile_type"
+            options={["STD", "PRF"]}
+            onChange={(e) => onChange(e)}
+            required
+          >
+            <option value="STD" key="STD">
+              Student
+            </option>
+            <option value="PRF" key="PRF">
+              Professor
+            </option>
+          </select>
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Register
+        </button>
+      </form>
+      <p className="mt-3">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { signup })(Signup);
